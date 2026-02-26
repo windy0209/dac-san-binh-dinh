c57ff89507ffe905417f55a8947b5d48dcabc277
windy0209 <miendatvo86@gmail.com>
2026-02-27 00:15:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,9 +25,6 @@ st.markdown("""
 /* Ẩn header trên cùng (Fork, GitHub, 3 chấm) */
 header {visibility: hidden !important;}
 
-/* Ẩn toolbar */
-[data-testid="stToolbar"] {display: none !important;}
-
 /* Ẩn menu 3 chấm */
 button[kind="header"] {display: none !important;}
 
@@ -396,4 +393,5 @@ elif chon_menu == "📞 Thông Tin":
     with col_map:
         toa_do = pd.DataFrame({'lat': [13.8930853], 'lon': [109.1002733]})
         st.map(toa_do, zoom=14)
+
 
