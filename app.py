e15c522d2fd471d3808725a85f1e4b0bf5670bbf
windy0209 <miendatvo86@gmail.com>
2026-03-03 23:49:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -651,7 +651,7 @@ elif chon_menu == "🛒 Giỏ Hàng":
             @st.cache_data
             def load_dia_chi():
                 # url = "https://raw.githubusercontent.com/windy0209/dac-san-binh-dinh/main/dia_chi.json"
-                  url = "https://raw.githubusercontent.com/windy0209/dac-san-binh-dinh/main/dia_chi.json"
+                  url = "https://github.com/windy0209/dac-san-binh-dinh/blob/main/dia_chi.json"
                 try:
                     response = requests.get(url)
                     response.raise_for_status()
@@ -1121,6 +1121,7 @@ st.markdown("""
     </a>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
